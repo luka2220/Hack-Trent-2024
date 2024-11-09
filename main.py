0cb77a536d12510708d15d2e013c70aef3e8bf3c
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-11-08 23:28:19 -0500

    dev/init-google-oauth init

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
-from flask import Flask
+from flask import Flask, url_for
+from dotenv import load_dotenv
+from authlib.integrations.flask_client import OAuth
+import os
 
+# Load all environment variables
+load_dotenv(".env")
+FLASK_SECRET = os.getenv("FLASK_SECRET")
+GOOGLE_OAUTH_CLIENT_ID = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
+GOOGLE_OAUTH_SECRET = os.getenv("GOOGLE_OAUTH_SECRET")
 app = Flask(__name__)
+app.secret_key = FLASK_SECRET
+
+# Initialize google oauth
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=GOOGLE_OAUTH_CLIENT_ID,
+    client_secret=GOOGLE_OAUTH_SECRET,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    client_kwargs={'scope': 'openid profile email'}
+)
 
 
 # Routes
 @app.route("/")
 def home():
-    return {"message": "home page", "status": 200}
+    return {"message": "home route", "status": 200}
 
 
 @app.route("/login")
 def login():
-    return {"message": "login route", "status": 200}
+    redirect_uri = url_for('login_callback', _external=True)
+    return google.authorize_redirect(redirect_uri)
 
 
 @app.route("/login/callback")
 def login_callback():
-    return {"message": "login redirect route from google", "status": 200}
+    token = google.authorize_access_token()
+    user_info = google.get('userinfo').json()
+    return 'Logged in as: ' + user_info['email']
 
 
 @app.route("/logout")

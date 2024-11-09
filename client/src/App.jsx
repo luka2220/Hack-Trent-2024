b0a5e6e7fccdfbd4e44c59a0cceb13ce15938361
yhou294 <yuting.yh@gmail.com>
2024-11-09 08:39:03 -0500

    added homepage, added link from landing to homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import AnchorButton from "./components/AnchorButton";
 
 function App() {
+  //render title, learn button and sign in button
   return (
     <>
       <h1>Hack Trent 24</h1>
+      <div className="buttons">
+        <AnchorButton
+          reference=""
+          text="Learn more"
+        />
+        <AnchorButton
+          reference="Home"
+          text="login"
+        />
+      </div>
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import Home from "./Pages/Home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "Home",
+    element: <Home />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
